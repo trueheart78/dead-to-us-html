a1b0de9968ad956e4427363de0936fb2e3de3f2b
Josh Mills <josh@trueheart78.com>
2021-06-02 15:00:32 -0400

    Extracted the Mustache template into assets/templates/corpses.mustache

--- html/assets/javascript/populate-cemetery.js
+++ html/assets/javascript/populate-cemetery.js
@@ -1,5 +1,9 @@
 function populateCemetery(bodies) {
-  var template = document.getElementById('template').innerHTML;
-  var rendered = Mustache.render(template, bodies);
-  document.getElementById('cemetery').innerHTML = rendered;
+  // var template = document.getElementById('template').innerHTML;
+  fetch('/assets/templates/corpses.mustache')
+    .then((response) => response.text())
+    .then((template) => {
+      var rendered = Mustache.render(template, bodies);
+      document.getElementById('cemetery').innerHTML = rendered;
+    });
 }
